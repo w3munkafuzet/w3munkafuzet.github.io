57cee99bac347e98d50242ec32799e9b90a85959
w3munkafuzet <44232637+w3munkafuzet@users.noreply.github.com>
2018-10-17 19:48:24 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -2,82 +2,102 @@ var initialData = {
   "HTML": [
     // --- Paragraph ---
     {
-      "title": "Paragraph", "done": false,
+      "title": "Paragraph", "done": false, "time": "(10-15 perc)",
       "themes": [
-        ""
+        "Tags: <p>, <br>"
       ],
       "questions": [
-        "Készíts egy külön mappát a terminálon keresztül! (cd, ls, mkdir, touch, pwd...)",
-        "A mappába csinálj egy index.html file-t."
+        "Készíts egy külön mappát a terminálon keresztül \"paragraph\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben próbáld ki a w3schools-on lévő példákat.",
+        "Bármilyen banálisnak tűnő információt, kérdést írj le!",
+        "Mikor visszatérsz, ez alapján fogod nekem elmesélni, mit kell ezekről tudni.",
+        "(Tényleg mindent írj le, azt is ami \"egyértelmű\" - nem érteni kell, hanem tudni)."
       ],
-      "help": "", "notes": ""
+      "help": "https://www.w3schools.com/html/html_paragraphs.asp", "notes": ""
     },
     // --- Paragraph ---
 
     // --- Link ---
     {
-      "title": "Link", "done": false,
+      "title": "Link", "done": false, "time": "(20-30 perc)",
       "themes": [
-        ""
+        "Tags: <a>",
+        "Attributes: href=, target=, title="
       ],
       "questions": [
-        "Készíts egy külön mappát a terminálon keresztül! (cd, ls, mkdir, touch, pwd...)",
-        "A mappába csinálj egy index.html file-t."
+        "Készíts egy külön mappát a terminálon keresztül \"link\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben próbáld ki a w3schools-on lévő példákat.",
+        "Írd le, mit kell tudni, az <a> tag-ről, illetve a fent említett attribute-okról.",
+        "Mikor visszatérsz, ez alapján fogod elmondani, hogy mit kell tudni például a \"href=\"-ről.",
+        "Ha valami nem világos, nem baj, sőt... Írd le akkor a kérdést."
       ],
-      "help": "", "notes": ""
+      "help": "https://www.w3schools.com/html/html_links.asp", "notes": ""
     },
     // --- Link ---
 
-    // --- Division ---
+    // --- Image ---
     {
-      "title": "Division", "done": false,
+      "title": "Image", "done": false, "time": "(10-15 perc)",
       "themes": [
-        ""
+        "Tags: <img>",
+        "Attributes: src=, alt=, width=, height="
       ],
       "questions": [
-        "Készíts egy külön mappát a terminálon keresztül! (cd, ls, mkdir, touch, pwd...)",
-        "A mappába csinálj egy index.html file-t."
+        "Készíts egy külön mappát a terminálon keresztül \"image\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben próbáld ki a w3schools-on lévő példákat.",
+        "Írd le, mit kell tudni, az <img> tag-ről, illetve a fent említett attribute-okról.",
+        "Mikor visszatérsz, ez alapján fogod elmondani, hogy mit kell tudni például a \"alt=\"-ról.",
+        "Itt is igaz - ha olvasva egyértelműnek is tűnik... írd le.",
+        "Ezeket még tudni kell, nem érteni."
       ],
-      "help": "", "notes": ""
+      "help": "https://www.w3schools.com/html/html_images.asp", "notes": ""
     }
-    // --- Division ---
+    // --- Image ---
   ],
   "CSS": [
+    // --- Text ---
     {
-      "title": "Paragraph", "done": false,
+      "title": "Text", "done": false, "time": "(15-20 perc)",
       "themes": [
-        ""
+        "color",
+        "text-align",
+        "text-decoration",
+        "A fentiek lehetséges értékei (pl: red, right, underline...)"
       ],
       "questions": [
-        "Készíts egy külön mappát a terminálon keresztül! (cd, ls, mkdir, touch, pwd...)",
-        "A mappába csinálj egy index.html file-t."
+        "Készíts egy külön mappát a terminálon keresztül \"text\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Készíts egy style.css file-t és használd fel a html-ben.",
+        "(Puskázhatsz közösen csinált anyagokból is, hogy kell.)",
+        "Mikor visszatérsz, a jegyzeteid alapján mondod majd el, mit tudsz e fenti témákról.",
+        "(Nem kell \"bemagolni\", épp ezért jegyzeteled le - majd idővel meg is marad.)"
       ],
-      "help": "", "notes": ""
+      "help": "https://www.w3schools.com/css/css_text.asp", "notes": ""
     },
+    // --- Text ---
 
+    // --- Background ---
     {
-      "title": "Paragraph", "done": false,
+      "title": "Background", "done": false, "time": "(10-15 perc)",
       "themes": [
-        ""
+        "background",
+        "background-color",
+        "background-image"
       ],
       "questions": [
-        "Készíts egy külön mappát a terminálon keresztül! (cd, ls, mkdir, touch, pwd...)",
-        "A mappába csinálj egy index.html file-t."
+        "Készíts egy külön mappát a terminálon keresztül \"background\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Készíts egy style.css file-t és használd fel a html-ben.",
+        "(Puskázhatsz közösen csinált anyagokból is, hogy kell.)",
+        "Mikor visszatérsz, a jegyzeteid alapján mondod majd el, mit tudsz e fenti témákról.",
+        "(Nem kell \"bemagolni\", épp ezért jegyzeteled le - majd idővel meg is marad.)"
       ],
-      "help": "", "notes": ""
+      "help": "https://www.w3schools.com/css/css_background.asp", "notes": ""
     },
-
-    {
-      "title": "Paragraph", "done": false,
-      "themes": [
-        ""
-      ],
-      "questions": [
-        "Készíts egy külön mappát a terminálon keresztül! (cd, ls, mkdir, touch, pwd...)",
-        "A mappába csinálj egy index.html file-t."
-      ],
-      "help": "", "notes": ""
-    }
+    // --- Background ---
   ],
   "Javascript": [
     // TODO
