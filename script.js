373132e20c543ca7c69dea17124300c975af80b7
w3munkafuzet <44232637+w3munkafuzet@users.noreply.github.com>
2018-10-17 23:10:19 +0200

    last

--- script.js
+++ script.js
@@ -3,29 +3,7 @@ var app = new Vue({
   data: {
     chosenCategory: "HTML",
     chosenLesson: null,
-    data: null,
-    videoUp: false,
-    imageUp: false,
-    images: [
-      "images/vanlife1.jpg",
-      "images/vanlife2.jpg",
-      "images/vanlife3.jpg",
-      "images/vanlife4.jpg",
-      "images/vanlife5.jpg",
-      "images/vanlife6.jpg",
-      "images/vanlife7.jpg",
-      "images/vanlife8.jpg",
-      "images/vanlife9.jpg",
-      "images/vanlife10.jpg",
-      "images/vanlife11.jpg",
-      "images/vanlife12.jpg",
-      "images/vanlife13.jpeg",
-      "images/vanlife14.jpeg",
-      "images/vanlife15.jpg",
-      "images/vanlife16.jpg",
-      "images/vanlife17.jpg",
-      "images/vanlife18.jpg"
-    ]
+    data: null
   },
   methods: {
     setCategory: function(category) {
@@ -38,24 +16,8 @@ var app = new Vue({
     open: function(url) {
       window.open(url);
     },
-    openVideo: function() {
-      this.videoUp = true;
-    },
-    openImage: function() {
-      this.imageUp = true;
-    },
-    closeVideo: function() {
-      this.videoUp = false;
-    },
-    closeImage: function() {
-      this.imageUp = false;
-    },
     update: function() {
       localStorage.setItem('data', JSON.stringify(this.data));
-    },
-    getRandomImage() {
-      var image = this.images[Math.floor(Math.random()*this.images.length)];
-      return image;
     },
     toggleDone: function(to) {
       this.data[this.chosenCategory][this.chosenLesson].done = to;
