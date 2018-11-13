7ccc9581cdf2d5f73d2aec5c3e931d205a37ac7a
w3munkafuzet <44232637+w3munkafuzet@users.noreply.github.com>
2018-11-13 18:46:05 +0100

    Add files via upload

---
+++ lastJslessData.js
@@ -0,0 +1,250 @@
+var initialData = {
+  "HTML": [
+    // --- Paragraph ---
+    {
+      "title": "Paragraph", "done": false, "time": "(10-15 perc)",
+      "themes": [
+        "Tags: <p>, <br>"
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"paragraph\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben próbáld ki a w3schools-on lévő példákat.",
+        "Bármilyen banálisnak tűnő információt, kérdést írj le!",
+        "Mikor visszatérsz, ez alapján tudod elmesélni, mit tudsz ezekről.",
+        "(Tényleg mindent írj le, azt is ami \"egyértelmű\" - nem érteni kell, hanem tudni)."
+      ],
+      "help": "https://www.w3schools.com/html/html_paragraphs.asp", "notes": ""
+    },
+    // --- Paragraph ---
+
+    // --- Link ---
+    {
+      "title": "Link", "done": false, "time": "(20-30 perc)",
+      "themes": [
+        "Tags: <a>",
+        "Attributes: href=, target=, title="
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"link\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben próbáld ki a w3schools-on lévő példákat.",
+        "Írd le, mit kell tudni, az <a> tag-ről, illetve a fent említett attribute-okról.",
+        "Mikor visszatérsz, ez alapján tudod elmesélni, mit tudsz például a \"href=\"-ről.",
+        "Ha valami nem világos, nem baj, sőt... Írd le akkor a kérdést."
+      ],
+      "help": "https://www.w3schools.com/html/html_links.asp", "notes": ""
+    },
+    // --- Link ---
+
+    // --- Image ---
+    {
+      "title": "Image", "done": false, "time": "(10-15 perc)",
+      "themes": [
+        "Tags: <img>",
+        "Attributes: src=, alt=, width=, height="
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"image\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben próbáld ki a w3schools-on lévő példákat.",
+        "Írd le, mit kell tudni, az <img> tag-ről, illetve a fent említett attribute-okról.",
+        "Mikor visszatérsz, ez alapján tudod elmesélni, hogy mit tudsz például a \"alt=\"-ról.",
+        "Itt is igaz - ha olvasva egyértelműnek is tűnik... írd le.",
+        "Ezeket még tudni kell, nem érteni."
+      ],
+      "help": "https://www.w3schools.com/html/html_images.asp", "notes": ""
+    },
+    // --- Image ---
+
+    // --- Input ---
+    {
+      "title": "Input", "done": false, "time": "(20-30 perc)",
+      "themes": [
+        "Tags: <input>",
+        "Attributes: type="
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"input\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben próbáld ki a w3schools-on lévő példákat.",
+        "Írd le, milyen value-kat kaphat a \"type=\" attribute.",
+        "Mikor visszatérsz, ez alapján tudod elmesélni, hogy mit tudsz például a \"type=date\"-ről.",
+        "Sok attribute lesz, ami nem feltétlen lesz egyértelmű (name=, value=) - nem baj!",
+        "A <form> tag szintén újdonság lehet, azzal se kell sokat játszani - elég az input.",
+        "A type-okat próbáld ki, lehetőleg az összeset, ha valami nem világos, írd le a kérdést."
+      ],
+      "help": "https://www.w3schools.com/html/html_form_input_types.asp", "notes": ""
+    },
+    // --- Input ---
+
+    // --- Button ---
+    {
+      "title": "Button", "done": false, "time": "(5 perc)",
+      "themes": [
+        "Tags: <button>",
+        "Attributes: -"
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"button\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Az elkészített html-ben készíts egy buttont, amiben szöveg van, illetve egyet amiben kép.",
+        "Ha valami nem világos, írd le a kérdést!!"
+      ],
+      "help": "https://www.w3schools.com/tags/tag_button.asp", "notes": ""
+    },
+    // --- Button ---
+
+    // --- Division ---
+    {
+      "title": "Division", "done": false, "time": "(30 perc)",
+      "themes": [
+        "Tags: <div>, <span>",
+        "Attributes: class=, id=, style="
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"division\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Készíts egy css file-t is, és linkeld be a html-hez.",
+        "Írj verseket file-ba, de ne használj se <p>, se <br>, se <h1>... tag-eket.",
+        "Csak <div> és <span> tag-eket, ezeket pedig formázd a css-ben.",
+        "(Lásd még: CSS - \"Margin\" fejezete!)",
+        "Leírja az oldal, mire jók, de szerintem elsőre nem teljesen egyértelmű.",
+        "Próbáld meg a saját szavaiddal megfogalmazni, majd megbeszéljük, ha van kérdés írd fel."
+      ],
+      "help": "https://www.w3schools.com/html/html_blocks.asp", "notes": ""
+    },
+    // --- Division ---
+
+    // --- Textarea ---
+    {
+      "title": "Textarea", "done": false, "time": "(5 perc)",
+      "themes": [
+        "Tags: <textarea>",
+        "Attributes: placeholder=, disabled=, autofocus="
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"textarea\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Próbáld ki a fenti tag-et és az említett attribute-okat.",
+        "Nem lesz nehéz, de azért jegyzeteld le : )"
+      ],
+      "help": "https://www.w3schools.com/tags/tag_textarea.asp", "notes": ""
+    },
+    // --- Textarea ---
+
+    // --- Script ---
+    {
+      "title": "Script", "done": false, "time": "(10 perc)",
+      "themes": [
+        "Tags: <script>, <link>",
+        "Attributes: src=, type=, href="
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"allDocuments\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html, egy style.css és egy script.js file-t, és nyisd meg atom-ban.",
+        "Írd bele a script.js-be: console.log('valami');",
+        "Kösd össze a script.js-t az index.html-lel.",
+        "A böngészőben job klikk, vizsgálat, majd console, ha kiírta, hogy valami, akkor kész is vagy : )"
+      ],
+      "help": "https://www.w3schools.com/tags/tag_script.asp", "notes": ""
+    }
+    // --- Script ---
+
+  ],
+  "CSS": [
+    // --- Text ---
+    {
+      "title": "Text", "done": false, "time": "(15-20 perc)",
+      "themes": [
+        "color",
+        "text-align",
+        "text-decoration",
+        "A fentiek lehetséges értékei (pl: red, right, underline...)"
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"text\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Készíts egy style.css file-t és használd fel a html-ben.",
+        "(Puskázhatsz közösen csinált anyagokból is, hogy kell.)",
+        "Mikor visszatérsz, ezek alapján tudod elmesélni, mit tudsz a fenti témákról.",
+        "(Határozottan nem kell \"bemagolni\", épp ezért jegyzeteled le - majd idővel meg is marad.)"
+      ],
+      "help": "https://www.w3schools.com/css/css_text.asp", "notes": ""
+    },
+    // --- Text ---
+
+    // --- Background ---
+    {
+      "title": "Background", "done": false, "time": "(10-15 perc)",
+      "themes": [
+        "background",
+        "background-color",
+        "background-image"
+      ],
+      "questions": [
+        "Készíts egy külön mappát a terminálon keresztül \"background\" névvel! (cd, ls, mkdir, touch, pwd...)",
+        "A mappában csinálj egy index.html file-t, és nyisd meg atom-ban és a chrome-ban.",
+        "Készíts egy style.css file-t és használd fel a html-ben.",
+        "(Puskázhatsz közösen csinált anyagokból is, hogy kell.)",
+        "Mikor visszatérsz, ezek alapján tudod elmesélni, mit tudsz a fenti témákról.",
+        "(Határozottan nem kell \"bemagolni\", épp ezért jegyzeteled le - majd idővel meg is marad.)"
+      ],
+      "help": "https://www.w3schools.com/css/css_background.asp", "notes": ""
+    },
+    // --- Background ---
+
+    // --- Margin ---
+    {
+      "title": "Margin", "done": false, "time": "(10-15 perc)",
+      "themes": [
+        "margin",
+        "padding",
+        "border"
+      ],
+      "questions": [
+        "Akkor csináld, mikor a HTML-en belül a \"Division\" című fejezet már kész.",
+        "Használd azt a html, és ott a Div-eket próbáld egymástól megfelelő távolságra helyezni.",
+        "Ezen egyszer már átfutottunk, de attól még jegyzeteld ki és csináld végig szépen : )",
+        "(Ne csak a margint! Használd a paddingot és a bordert is, még ha picit erőltetett is)"
+      ],
+      "help": "https://www.w3schools.com/css/css_margin.asp", "notes": ""
+    },
+    // --- Margin ---
+
+    // --- Float ---
+    {
+      "title": "Margin", "done": false, "time": "(5 perc)",
+      "themes": [
+        "float"
+      ],
+      "questions": [
+        "Elég elővenned a \"Paragraph\"-os dokumentumot, ott próbáld ki.",
+        "Csinálj egy left és egy right float-ot, és kb meg is vagy : ).",
+        "Annyi jegyzetet azért csinálj (ha csak 1 sor is), hogy ez alapján el tudd majd mondani, mire jó : )"
+      ],
+      "help": "https://www.w3schools.com/css/css_float.asp", "notes": ""
+    },
+    // --- Float ---
+
+    // --- Position ---
+    {
+      "title": "Position", "done": false, "time": "(15-20 perc)",
+      "themes": [
+        "position - static, relative, absolute, fixed"
+      ],
+      "questions": [
+        "Ez lesz a CSS-en belül a legkomolyabb talán.",
+        "Csinálj külön mappát neki, bele html-t és css-t.",
+        "Csináld végig a példákat.",
+        "Ezután csinálj néhány <div>-et, és ezeket pakold ide-oda a képernyőn : )",
+        "Írd le, hogy mi jött át - ez picit komplexebb, majd megbeszéljük : )"
+      ],
+      "help": "https://www.w3schools.com/css/css_positioning.asp", "notes": ""
+    }
+    // --- Position ---
+  ],
+  "Javascript": [
+    // TODO
+  ]
+}
